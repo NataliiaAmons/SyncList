3e3ba292a1feed391ff270c3d367a40b3ee15912
NataliiaAmons <nataliya.amons@gmail.com>
2025-04-29 21:39:34 +0300

    Adding login verification by email and password

--- src/scripts/sign-up-and-log-in.js
+++ src/scripts/sign-up-and-log-in.js
@@ -1,4 +1,19 @@
-// Canges vidibility for password fields
+// Example users
+const users = [
+    {
+      username: "james_walker",
+      email: "james.walker@example.com",
+      password: "Jw@2024rock"
+    },
+    {
+      username: "sofia.reed",
+      email: "sofia.reed@example.com",
+      password: "S0f!aR33d#"
+    }
+]
+
+
+// Changes visibility for password fields
 function changeVisibility(elementId){
     const container = document.getElementById(elementId);
     const inputElement = container.querySelector("input")
@@ -14,3 +29,27 @@ function changeVisibility(elementId){
         eye_slash.classList.toggle("inline");
       
 }
+
+// LOG IN: Checks if input is correct
+document.getElementById("log-in-form").addEventListener("submit", function(event){
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
+
+    const error = document.getElementById("error");
+
+    error.classList.add("hidden");
+
+    const getUser = users.find(user => user.email === email)
+
+    if (!getUser || password != getUser.password) {
+        event.preventDefault();
+        error.classList.toggle("hidden");
+        error.innerText = "Wrong email or password";
+    }
+    else {
+        window.open("purchase.html");
+    }
+
+})
+
