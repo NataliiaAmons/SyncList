9aba875411ca860c6a622bfe0c0bfb83e80b801a
NataliiaAmons <nataliya.amons@gmail.com>
2025-05-20 01:39:48 +0300

    React: Adding Footer component

--- sync-list-react/src/App.js
+++ sync-list-react/src/App.js
@@ -1,11 +1,17 @@
-import './styles/global.css';
-import './styles/header.css';
-import './styles/color-scheme.css';
+import "./styles/global.css";
+import "./styles/color-scheme.css";
+import "./styles/header.css";
+import "./styles/footer.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Header></Header>
+    <div className="body">
+      <Header></Header>
+      <div className="content"></div>
+      <Footer></Footer>
+    </div>
   );
 }
 
--- sync-list-react/src/components/Header.js
+++ sync-list-react/src/components/Header.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 
-export default function Header(){
-    return (
-        <div className = "header bg-support border-bottom-gray">
+export default function Header() {
+  return (
+    <div className="header bg-support border-bottom-gray">
+      <div className="left-section">
+        <img className="logo-image" src="logo.png" alt="logo" />
+      </div>
 
-        <div className = "left-section">
-            <img className="logo-image" src="logo.png" alt="logo"/> 
-        </div>
+      <div className="middle-section">
+        <p className="header-link text-dark">All lists</p>
+        <p className="header-link text-dark">Claimed tasks</p>
+        <p className="header-link text-dark">Upcoming</p>
+        <p className="header-link text-dark">Friends</p>
+      </div>
 
-        <div className = "middle-section">
-            <p className="header-link text-dark">All lists</p>
-            <p className="header-link text-dark">Claimed tasks</p>
-            <p className="header-link text-dark">Upcoming</p>
-            <p className="header-link text-dark">Friends</p>
-        </div>
-
-        <div className = "right-section">
-            <img className="profile-picture" src="images\user-profile-picture.jpg" alt="profile"/>
-            <p className="username-header text-dark">Nataliia</p>
-        </div>
-        </div>
-    );
+      <div className="right-section">
+        <img
+          className="profile-picture"
+          src="images\user-profile-picture.jpg"
+          alt="profile"
+        />
+        <p className="username-header text-dark">Nataliia</p>
+      </div>
+    </div>
+  );
 }
